d23cd6c427986bc91fa5e84dd3377300477be61b
Jeff MacDonald <macjeffrey@gmail.com>
2023-01-16 20:59:38 -0600

    Update compare_json.py
    Went a different route to only get the different pair value commented out

--- compare_json.py
+++ compare_json.py
@@ -1,28 +1,16 @@
 import json
 
-def compare_json(file1, file2, output_file):
-    # load the two JSON files
-    with open(file1, 'r') as f1:
+def compare_json(file1, file2, new_file):
+    with open(file1, 'r') as f1, open(file2, 'r') as f2:
         data1 = json.load(f1)
-    with open(file2, 'r') as f2:
         data2 = json.load(f2)
-
-    # compare the two JSON files
-    differences = {}
-    for key in data1:
-        if key not in data2:
-            differences[key] = data1[key]
-        elif data1[key] != data2[key]:
-            differences[key] = data1[key]
-
-    for key in data2:
-        if key not in data1:
-            differences[key] = data2[key]
-
-    # write the differences to the output file
-    with open(output_file, 'w') as f:
-        for key in differences:
-            f.write('# ' + key + ': ' + str(differences[key]) + '\n')
-
-
-compare_json('file1.json', 'file2.json', 'differences.json')
+        differences = {}
+        for key in data1.keys():
+            if data1[key] != data2.get(key):
+                differences[key] = data2[key]
+        with open(new_file, 'w') as f:
+            for key in differences.keys():
+                f.write('# ' + key + ': ' + json.dumps(differences[key]) + '\n')
+            for key in data1.keys():
+                if key not in differences.keys():
+                    f.write(key + ': ' + json.dumps(data1[key]) + '\n')
